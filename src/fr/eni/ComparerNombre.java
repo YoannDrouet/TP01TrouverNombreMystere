018333fd61ab45378da9c2d83afd9dc3fdbb7a0a
YoannDrouet <yoanndrouet@hotmail.fr>
2021-06-09 08:27:05 +0200

    Initial commit

---
+++ src/fr/eni/ComparerNombre.java
@@ -0,0 +1,30 @@
+package fr.eni;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.Random;
+
+public class ComparerNombre extends HttpServlet {
+    private boolean trouver = false;
+    private int nombreMystere;
+
+    @Override
+    public void init() throws ServletException {
+        int borneMax = 10;
+        int borneMin = 0;
+        try {
+            borneMax = Integer.parseInt(this.getInitParameter("BORNE_MAX"));
+            borneMin = Integer.parseInt(this.getInitParameter("BORNE_MIN"));
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        nombreMystere = new Random().nextInt(borneMax+1-borneMin)+borneMin;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
